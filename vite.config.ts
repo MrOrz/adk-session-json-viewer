2df5c7c686e4912756daeb853adc65c7ba1e2ef5
MrOrz <johnsonliang7@gmail.com>
2026-02-02 14:04:45 +0800

    fix: add base for github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(() => {
       port: 3000,
       host: '0.0.0.0',
     },
+    base: '/adk-session-json-viewer/',
     plugins: [react()],
     resolve: {
       alias: {
